7b4d907b7f1a4cb5c887024440a1d2df7341fd4f
Ticoxxo <ticoxxo@gmail.com>
2024-09-09 11:39:39 -0700

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Alberto Almeida on 09/09/24.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
